45d7344f1d50c4e0f2bdf7655d47a30a03f9a802
Sean Sundberg <seansund@us.ibm.com>
2020-12-01 08:43:22 -0600

    Fixes logic to check for existing index.yaml catalog

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ PUBLISH_BRANCH="$4"
 "${SCRIPT_DIR}/scripts/build-module-metadata.sh" "${TAG_NAME}" "${DIST_DIR}" "${REPO}"
 cp README.md "${DIST_DIR}"
 
-if [[ $(curl -L "https://raw.githubusercontent.com/${REPO}/${PUBLISH_BRANCH}/index.yaml") != "404: Not Found" ]]; then
+if [[ $(curl -L "https://raw.githubusercontent.com/${REPO}/${PUBLISH_BRANCH}/index.yaml") == "404: Not Found" ]]; then
   echo "No existing catalog found"
 else
   curl -LO "https://raw.githubusercontent.com/${REPO}/${PUBLISH_BRANCH}/index.yaml"
